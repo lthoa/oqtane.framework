e224671a72fea849d00f410e308204a6a20808ab
Le Thanh Hoa <lthoa@hotmail.com>
2024-05-20 20:51:22 +0700

    Update path

--- Oqtane.Shared/Shared/Utilities.cs
+++ Oqtane.Shared/Shared/Utilities.cs
@@ -59,7 +59,7 @@ namespace Oqtane.Shared
             var uriBuilder = new UriBuilder
             {
                 Path = !string.IsNullOrEmpty(alias)
-                    ? (!string.IsNullOrEmpty(path)) ? $"{alias}/{path}": $"{alias}"
+                    ? (!string.IsNullOrEmpty(path)) ? $"{alias}{path}": $"{alias}"
                     : $"{path}",
                 Query = querystring,
                 Fragment = fragment
