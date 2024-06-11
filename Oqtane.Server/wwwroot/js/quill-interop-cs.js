d512d3fcddff52dff580d371b328c9722c4824e4
Le Thanh Hoa <lthoa@hotmail.com>
2024-06-11 09:21:52 +0700

    ok

--- Oqtane.Server/wwwroot/js/quill-interop-cs.js
+++ Oqtane.Server/wwwroot/js/quill-interop-cs.js
@@ -1,19 +1,14 @@
 var CS = CS || {};
 
-CS.RichTextEditor = {
-    getCurrentCursor: function (quillElement, html) {
-        var Delta = Quill.import('delta');
-        editorIndex = 0;
-        if (quillElement.__quill.getSelection() !== null) {
-            editorIndex = quillElement.__quill.getSelection().index;
+CS.RichTextEditor = {    
+    insertQuillImage: function (elementId, imageURL, altText, editorIndex) {        
+        var element = document.getElementById(elementId);
+        if (element !== null) {
+            var Delta = Quill.import('delta');
+            return quillElement.__quill.updateContents(
+                new Delta()
+                    .retain(editorIndex)
+                    .insert({ image: imageURL }, { alt: altText }));
         }
-        return editorIndex;
-    },
-    insertQuillImage: function (quillElement, imageURL, altText, editorIndex) {
-        var Delta = Quill.import('delta');
-        return quillElement.__quill.updateContents(
-            new Delta()
-                .retain(editorIndex)
-                .insert({ image: imageURL }, { alt: altText }));
     }
 };
